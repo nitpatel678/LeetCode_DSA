d73690643f4a7b1f46fd34cd0745be178179d877
Nitin Patel <nitpatel678@gmail.com>
2025-02-15 22:11:44 +0530

    Update

---
+++ LinkedList/RemoveElements.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class RemoveElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Close the scanner to free resources
+        sc.close();
+    }
+}
