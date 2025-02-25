4f63c081d5796de93a22a734e428c2fd7272314a
Nitin Patel <nitpatel678@gmail.com>
2025-02-25 21:15:57 +0530

    Update

--- String/LongestPalindrome.java
+++ String/LongestPalindrome.java
@@ -1,12 +1,14 @@
 import java.util.*;
-public class LongestPalindrome{
+
+public class LongestPalindrome {
 
     class Solution {
         public String longestPalindrome(String s) {
-            if (s.length() <= 1) return s;
-    
+            if (s.length() <= 1)
+                return s;
+
             int start = 0, maxLen = 0;
-    
+
             for (int i = 0; i < s.length(); i++) {
                 for (int j = i; j < s.length(); j++) {
                     if (isPalindrome(s, i, j) && (j - i + 1) > maxLen) {
@@ -15,10 +17,10 @@ public class LongestPalindrome{
                     }
                 }
             }
-    
+
             return s.substring(start, start + maxLen);
         }
-    
+
         private boolean isPalindrome(String s, int left, int right) {
             while (left < right) {
                 if (s.charAt(left++) != s.charAt(right--)) {
@@ -28,7 +30,6 @@ public class LongestPalindrome{
             return true;
         }
     }
-    
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
